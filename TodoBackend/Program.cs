0e29991ec882b3a6959e8a4aa71b2faae906f687
eso8086 <elicodingyt@gmail.com>
2025-06-19 19:19:21 +0400

    basic CRUD operations

--- TodoBackend/Program.cs
+++ TodoBackend/Program.cs
@@ -1,5 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using TodoBackend;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,33 +8,60 @@ builder.Services.AddDbContext<TodoDb>(options => options.UseInMemoryDatabase("To
 
 var app = builder.Build();
 
-app.MapPost("/todo", async (TodoDb db, Todo newTodo) =>
+app.MapGet("/todos", async (TodoDb db) =>
 {
-    var added = await db.Todos.AddAsync(newTodo);
-    await db.SaveChangesAsync();
-    return added.Entity;
+    return await db.Todos.ToArrayAsync();
 });
 
-app.MapPatch("/todo", async (TodoDb db, Dictionary<string, int> requestBody) =>
+app.MapGet("/todos/complete", async (TodoDb db) =>
 {
-    int id = requestBody["id"];
-    Todo? todo = await db.Todos.FirstOrDefaultAsync(t=>t.Id == id);
+    return await db.Todos.Where(t => t.IsComplete).ToListAsync();
+});
 
+app.MapGet("/todos/{id}", async (int id, TodoDb db) =>
+{
+    return await db.Todos.FindAsync(id)
+        is Todo todo
+        ? Results.Ok(todo) 
+        : Results.NotFound();
+});
+
+app.MapPost("/todos", async (TodoDb db, Todo newTodo) =>
+{
+    await db.Todos.AddAsync(newTodo);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todos/{newTodo.Id}", newTodo);
+});
+
+// TODO: handle invalid inputTodo content
+app.MapPut("/todos/{id}", async (int id, TodoDb db, Todo inputTodo) =>
+{
+    Todo? todo = await db.Todos.FindAsync(id);
+    
     if (todo is null)
     {
         return Results.NotFound();
     }
-        
-    todo.IsComplete = true;
-    db.Todos.Update(todo);
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+  
     await db.SaveChangesAsync();
     
     return Results.Ok();
 });
 
-app.MapGet("/todo", async (TodoDb db) =>
+app.MapDelete("todos/{id}", async (int id, TodoDb db) =>
 {
-    return db.Todos.ToArray();
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+    
+    return Results.NotFound();
 });
+
 
 app.Run();
